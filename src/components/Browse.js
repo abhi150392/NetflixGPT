e03059bfc257a53930b29d101c6fa84b781ab7ef
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-08-30 21:23:34 -0500

    Modified UI

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,23 @@
 import useNpwPlayingMovies from "../hooks/useNpwPlayingMovies";
 import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNpwPlayingMovies();
   return (
     <div>
       <Header />
+      {/* 
+        - Main Container
+          - Video Background
+          - Video Title
+        - Secondary Container
+          - MovieList * n
+            - card * n      
+      */}
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
